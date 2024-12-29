2a6c20a93356694e4db6a0059e6d58e37164c81c
yash <yashchauhan1775@gmail.com>
2024-12-29 16:23:03 +0530

    navbar animation added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,86 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/component.css";
 import logo from "../assets/logo.png";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { useNavigate } from "react-router-dom";
-import { setMyAuth } from "../utils/helpers";
+import gsap from "gsap";
 
-const notify = () => {
-  toast("🦄 Wow so easy!", {
-    position: "bottom-center",
-    autoClose: 5000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-    transition: Bounce,
-  });
-};
+// import image1 from "../assets/c++.png";
+// import image2 from "../assets/c.png";
+// import image3 from "../assets/dart.png";
+// import image4 from "../assets/flutter.png";
+import base from "../assets/sb.jpeg";
+import cs from "../assets/cs.jpeg";
+import cn from "../assets/cn.jpeg";
+import py from "../assets/py.jpeg";
+import mern from "../assets/mern.jpeg";
+import dm from "../assets/dm.jpeg";
+import srs from "../assets/srs.jpeg";
+import ml from "../assets/ml.jpeg";
+
 
 const Navbar = () => {
   const { user, logout } = useKindeAuth();
+  const [menuTGL, setMenuTGL] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [hoveredImage, setHoveredImage] = useState(base);
+  const [isImageChanging, setIsImageChanging] = useState(false); // Animation state
   const navigate = useNavigate();
 
-  const isAdmin = sessionStorage.getItem("isAdmin") === "true";
+  function menuToggle() {
+    if (!menuTGL) {
+      setIsVisible(true);
+      setMenuTGL(true);
+    } else {
+      setMenuTGL(false);
+    }
+  }
+
+  useEffect(() => {
+    if (menuTGL) {
+      const tl = gsap.timeline();
+      tl.fromTo(
+        ".menu-cnt",
+        { y: "100%", opacity: 0 },
+        { y: "0%", opacity: 1, duration: 0.5 }
+      );
+      tl.fromTo(
+        ".menu-item",
+        { y: "50px", opacity: 0 },
+        { y: "0", opacity: 1, duration: 0.3, stagger: 0.1, ease: "power2.out" },
+        "-=0.2"
+      );
+      tl.fromTo(
+        ".menu-cnt-right",
+        { y: "400px", opacity: 0 },
+        { y: "0", opacity: 1, duration: 0.3, stagger: 0.1, ease: "power2.out" },
+        "-=0.9"
+      );
+    } else if (!menuTGL && isVisible) {
+      const tl = gsap.timeline({
+        onComplete: () => setIsVisible(false),
+      });
+      tl.to(".menu-item", {
+        y: "50px",
+        opacity: 0,
+        duration: 0.3,
+        stagger: 0.05,
+        ease: "power2.in",
+      });
+      tl.to(".menu-cnt", { y: "100%", opacity: 0, duration: 1 }, "-=0.2");
+      tl.to(".menu-cnt-right", { y: "100%", opacity: 0, duration: 1 }, "-=1.3");
+    }
+  }, [menuTGL, isVisible]);
+
+  const handleHover = (image) => {
+    setIsImageChanging(true); // Trigger animation
+    setTimeout(() => {
+      setHoveredImage(image);
+      setIsImageChanging(false); // Reset animation state
+    }, 300); // Match transition duration
+  };
 
   return (
     <>
@@ -45,39 +100,83 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="lnav">
           <img src={logo} alt="logo" />
-          <h1 onClick={() => navigate('/auth')}>study buddy</h1>
+          <h1 onClick={() => navigate("/auth")}>study buddy</h1>
         </div>
         <div className="rnav">
           <div className="nav-btn">
-            {/* {isAdmin && (
-              <button className="btn1" onClick={() => navigate("/admin")}>
-                Admin Dashboard
-              </button>
-            )} */}
-            <div className="dropdown">
-              <button className="dropbtn">menu</button>
-              <div className="dropdown-content">
-                <a href="#">
+            <button className="btn2" onClick={() => menuToggle()}>
+              menu
+            </button>
+            {isVisible && (
+              <div className="menu-cnt">
+                <div className="menu-cnt-top">
+                  your resources
+                </div>
+                <div className="menu-cnt-left">
                   <button
-                    className="btn1"
-                    onClick={() =>{
-                      setMyAuth(false)
-                      logout({
-                        logoutParams: { returnTo: window.location.origin },
-                      })
-                    }}
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(srs)}
                   >
-                    Log Out
+                    <span>SRS</span>
                   </button>
-                </a>
+                  <button
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(cs)}
+                  >
+                    <span>syber security</span>
+                  </button>
+                  <button
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(ml)}
+                  >
+                    <span>machine learning</span>
+                  </button>
+                  <button
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(py)}
+                  >
+                    <span>python</span>
+                  </button>
+                  <button
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(cn)}
+                  >
+                    <span>computer networks</span>
+                  </button>
+                  <button
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(dm)}
+                  >
+                    <span>data mining</span>
+                  </button>
+                  <button
+                    className="menu-item"
+                    onMouseEnter={() => handleHover(mern)}
+                  >
+                    <span>mern projects</span>
+                  </button>
+                </div>
+                <div className="menu-cnt-right">
+                  {hoveredImage && (
+                    <img
+                      src={hoveredImage}
+                      alt={hoveredImage}
+                      className={`hovered-image ${
+                        isImageChanging ? "fade-out" : "fade-in"
+                      }`}
+                    />
+                  )}
+                </div>
               </div>
-            </div>
+            )}
           </div>
-          {/* <h3>{user.given_name}</h3> */}
           <div style={{ height: "100%", borderLeft: "1px solid white" }}>
             &nbsp;
           </div>
-          <div className="nav-profile" onClick={()=>navigate('/auth/profile')}>
+          <div
+            className="nav-profile"
+            onClick={() => navigate("/auth/profile")}
+          >
             <img src={user.picture} alt="profile" />
           </div>
         </div>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,7 @@ import Loader from "../components/Loader";
 import "../css/profile.css";
 
 const Profile = () => {
-  const { user } = useKindeAuth();
+  const { user,logout } = useKindeAuth();
 
   if (!user) {
     return <Loader />;
@@ -20,6 +20,11 @@ const Profile = () => {
           </h1>
           <p>{user.email}</p>
         </div>
+      </div>
+      <div style={{ textAlign: "center" }} >
+        <button className="btn2" onClick={logout}>
+          log out
+        </button>
       </div>
     </>
   );
